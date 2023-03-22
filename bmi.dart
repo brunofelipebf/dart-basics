ec0f36a4fa52bf30ee6038cf4d90a8d76b8f4354
Bruno <bfsilva98@gmail.com>
2023-03-22 14:05:09 -0300

    final version

---
+++ bmi.dart
@@ -0,0 +1,31 @@
+import 'dart:io';
+import 'dart:math';
+
+main(){
+    print('Type your weight in kilograms:');
+    var weight = double.parse(stdin.readLineSync()!);
+
+    print('Type your height in meters separated by dot e.g 1.88:');
+    var height = double.parse(stdin.readLineSync()!);
+
+    var calc = weight / pow(height, 2);
+
+    if(calc < 18.5){
+        print('Underweight');
+    } 
+    else if(calc >= 18.5 && calc <= 24.9){
+        print('Normal weight');
+    } 
+    else if(calc >= 25 && calc <= 29.9){
+        print('Overweight');
+    } 
+    else if(calc >= 30 && calc <= 34.9){
+        print('Obesity type I');
+    } 
+    else if(calc >= 35 && calc <= 39.9){
+        print('Obesity type II');
+    } 
+    else{
+        print('Obesity type III');
+    }
+}
