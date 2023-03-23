55b20a79b1b595a35c164d605e48d29643a3ed16
Bruno <bfsilva98@gmail.com>
2023-03-23 14:52:17 -0300

    added remove opt

--- shoppingList.dart
+++ shoppingList.dart
@@ -16,6 +16,14 @@ main(){
                 print('item $i - ${products[i]}');
             }
         }
+        else if(text == 'remove'){
+            print('which item would you like to remove?');
+            for(var i = 0; i < products.length; i++){
+                print('item $i - ${products[i]}');
+            }
+            var position = int.parse(stdin.readLineSync()!);
+            products.removeAt(position);
+        }
         else{
         products.add(text);
         }
