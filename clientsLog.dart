2149aec7f5fb41dcae86e2775c916acca5700d66
Bruno <bfsilva98@gmail.com>
2023-03-27 20:56:02 -0300

    multiple signups

--- clientsLog.dart
+++ clientsLog.dart
@@ -1,5 +1,6 @@
 import 'dart:io';
 
+List<Map<String, dynamic>> signups = [];
 Map<String, dynamic> clientsLog = {};
 void main() {
   bool condition = true;
@@ -9,10 +10,13 @@ void main() {
     var command = stdin.readLineSync();
     if (command == "quit") {
       print('finished program');
+      condition = false;
     } else if (command == 'signup') {
       signup();
     } else if (command == 'print') {
       print(clientsLog);
+    } else if (command == 'print all') {
+      print(signups);
     }
   }
 }
@@ -25,4 +29,6 @@ signup() {
   print('type your age');
   var age = stdin.readLineSync();
   clientsLog['age'] = age;
+
+  signups.add(clientsLog);
 }
